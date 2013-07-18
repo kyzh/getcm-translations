d8c5eff1ff6bfab4d4bc05a6b033df532870e7de
Florentin Raud <florentin.raud@gmail.com>
2013-07-18 08:43:02 +0100

    Create fr-fr.js
    Just a French locale version

---
+++ src/fr-fr.js
@@ -0,0 +1,17 @@
+(function() {
+  var getcm = window.getcm || { locales:{} };
+
+  _.extend(getcm.locales, { "fr-FR": {
+    navbar: {
+      about:"A propos",
+      download:"Téléchargement",
+      devices:"Appareils",
+      blog:"Blog",
+      forum:"Forum",
+      community:"Communauté",
+      manufacturers:"Fabricants",
+      all_manufacturers:"Tout les fabricants"
+    }
+  }});
+
+}).call(this);
