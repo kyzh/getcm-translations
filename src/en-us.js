892857408fd05fcc93491fea0d227922bebdd825
Chris Soyars <ctso@ctso.me>
2013-07-17 21:49:09 -0700

    Initial Commit

---
+++ src/en-us.js
@@ -0,0 +1,17 @@
+(function() {
+  var getcm = window.getcm || { locales:{} };
+
+  _.extend(getcm.locales, { "es-ES": {
+    navbar: {
+      about:"About",
+      download:"Download",
+      devices:"Devices",
+      blog:"Blog",
+      forum:"Forum",
+      community:"Community",
+      manufacturers:"Manufacturers",
+      all_manufacturers:"All Manufacturers"
+    }
+  }});
+
+}).call(this));
---
+++ src/es-es.js
@@ -0,0 +1,17 @@
+(function() {
+  var getcm = window.getcm || { locales:{} };
+
+  _.extend(getcm.locales, { "es-ES": {
+    navbar: {
+      about:"Sobre",
+      download:"Descargar",
+      devices:"Dispositivos",
+      blog:"Blog",
+      forum:"Foro",
+      community:"Comunidad",
+      manufacturers:"Fabricantes",
+      all_manufacturers:"Todos los fabricantes"
+    }
+  }});
+
+}).call(this));
